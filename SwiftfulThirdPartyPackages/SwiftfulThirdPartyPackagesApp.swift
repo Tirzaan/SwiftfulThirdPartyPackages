5b391eee260bd6983197e2c0750f5f4094ec9453
Tirzaan Neil <Tirzaan@tntneil.com>
2025-10-21 12:40:15 -0600

    Initial Commit

---
+++ SwiftfulThirdPartyPackages/SwiftfulThirdPartyPackagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThirdPartyPackagesApp.swift
+//  SwiftfulThirdPartyPackages
+//
+//  Created by Tirzaan on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThirdPartyPackagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
