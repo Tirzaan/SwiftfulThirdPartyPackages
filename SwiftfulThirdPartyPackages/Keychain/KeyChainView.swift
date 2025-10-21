0a81d76f743193ad3a3f493c9e988e554aee6603
Tirzaan Neil <Tirzaan@tntneil.com>
2025-10-21 15:38:36 -0600

    [Episode] 3 - Keychain-swift

---
+++ SwiftfulThirdPartyPackages/Keychain/KeyChainView.swift
@@ -0,0 +1,18 @@
+//
+//  KeyChainView.swift
+//  SwiftfulThirdPartyPackages
+//
+//  Created by Tirzaan on 10/21/25.
+//
+
+import SwiftUI
+
+struct KeyChainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KeyChainView()
+}
